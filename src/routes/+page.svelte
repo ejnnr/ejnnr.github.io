6e38169ab845059829ff7ed116caa04985a44307
Erik Jenner <erik.jenner99@gmail.com>
2025-06-03 23:01:58 -0700

    Replace inline SVG icons with unplugin-icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { marked } from 'marked';
+	import IconX from '~icons/simple-icons/x';
+	import IconGoogleScholar from '~icons/simple-icons/googlescholar';
+	import IconGithub from '~icons/simple-icons/github';
 	// You can add any JavaScript logic here if needed
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -38,12 +41,7 @@
 						class="text-gray-400 hover:text-gray-500"
 					>
 						<span class="sr-only">Twitter</span>
-						<!-- Twitter Icon -->
-						<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-							<path
-								d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
-							/>
-						</svg>
+						<IconX class="h-6 w-6" />
 					</a>
 					<a
 						href="https://scholar.google.com/citations?user=8DgF8HcAAAAJ"
@@ -52,12 +50,7 @@
 						class="text-gray-400 hover:text-gray-500"
 					>
 						<span class="sr-only">Google Scholar</span>
-						<!-- Google Scholar Icon -->
-						<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-							<path
-								d="M12 24c-6.627 0-12-5.373-12-12s5.373-12 12-12 12 5.373 12 12-5.373 12-12 12zm0-22c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10-4.477-10-10-10zm-3.6 15.187l3.6-2.113 3.6 2.113-1.377-4.074 3.08-2.667h-3.802l-1.501-4.286-1.501 4.286h-3.802l3.08 2.667-1.377 4.074z"
-							/>
-						</svg>
+						<IconGoogleScholar class="h-6 w-6" />
 					</a>
 					<a
 						href="https://github.com/ejnnr"
@@ -66,14 +59,7 @@
 						class="text-gray-400 hover:text-gray-500"
 					>
 						<span class="sr-only">GitHub</span>
-						<!-- GitHub Icon -->
-						<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-							<path
-								fill-rule="evenodd"
-								d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.5.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.026 2.747-1.026.546 1.379.201 2.397.1 2.65.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.523 2 12 2z"
-								clip-rule="evenodd"
-							/>
-						</svg>
+						<IconGithub class="h-6 w-6" />
 					</a>
 				</div>
 			</div>
